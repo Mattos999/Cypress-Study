64627f3feb461413802f9abedc91ab5f31ab2982
Carlos Matos <152405634+Mattos999@users.noreply.github.com>
2024-09-02 13:59:46 -0300

    Update spec.cy.js

--- spec.cy.js
+++ spec.cy.js
@@ -6,15 +6,15 @@ describe('template spec', () => {
     cy.contains('a', 'Cadastrar').click();
     //preencher o Formulário para cadastro
     cy.get('input[name = "nome"]').type('Carlos Matos');
-    cy.get('input[name = "email"]').type('carlos.matos.nac@gmail.com');
-    cy.get('input[name = "password"]').type('Matos150615');
-    cy.get('input[name = "confirm_password"]').type('Matos150615');
+    cy.get('input[name = "email"]').type('test@gmail.com');
+    cy.get('input[name = "password"]').type('test12345');
+    cy.get('input[name = "confirm_password"]').type('test12345');
     //Se cadastrar
     cy.contains('button', 'Cadastrar').click();
     //Logar
     //Preencher dados de Login
-    cy.get('input[name = "email"]').type('carlos.matos.nac@gmail.com');
-    cy.get('input[name = "password"]').type('Matos150615');
+    cy.get('input[name = "email"]').type('test@gmail.com');
+    cy.get('input[name = "password"]').type('test12345');
     //Clicar em Entrar
     cy.contains('button', 'Entrar').click();
   })
